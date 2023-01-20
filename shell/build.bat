15cd11440ff954780cb717a38d5cebc3f2d91dc3
Unbox101 <samuelversteegh@gmail.com>
2023-01-20 06:55:17 -0600

    There was a problem
    There was a problem with the fake preprocessor hack i made. Now it's no longer a hack. It's real. and now works.

--- shell/build.bat
+++ shell/build.bat
@@ -1,4 +1,6 @@
 @echo off
 cls
+call "preprocess.bat"
+echo --Building N Running--
 nelua -C %proj_path%\src\Main.nelua
 
---
+++ shell/preprocess.bat
@@ -0,0 +1,4 @@
+@echo off
+echo --Preprocessing--
+nelua -C %proj_path%\src\Preprocess.nelua
+
